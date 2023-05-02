5cd5ade0ea5285120b693dc40a9dadbd2e312569
dheeraj <dheerajkumar@techis.io>
2023-05-03 02:10:37 +0530

    last upload

--- index.js
+++ index.js
@@ -37,85 +37,92 @@ app.post('/register', async (req, res) => {
     let result = await user.save();
     result = result.toObject();
     delete result.password
-    Jwt.sign({user} , jwtKey , {expiresIn: "2h"} , (err , token) => {
+    Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
         if (err) {
-            res.send({ result: "something went wrong"})
+            res.send({ result: "something went wrong" })
         }
-        res.send({user, auth: token})
+        res.send({ user, auth: token })
     })
 })
-    
-    
 
 
 
 
 
-app.post("/login", async (req ,res) => {
+
+
+app.post("/login", async (req, res) => {
     console.log(req.body)
     if (req.body.password && req.body.email) {
         let user = await User.findOne(req.body).select("-password");
         if (user) {
-            Jwt.sign({user} , jwtKey , {expiresIn: "2h"} , (err , token) => {
+            Jwt.sign({ user }, jwtKey, { expiresIn: "2h" }, (err, token) => {
                 if (err) {
-                    res.send({ result: "something went wrong"})
+                    res.send({ result: "something went wrong" })
                 }
-                res.send({user, auth: token})
+                res.send({ user, auth: token })
             })
-            
+
         } else {
-            res.send({ result: "No user Found"})
+            res.send({ result: "No user Found" })
         }
     }
-        else {
-            res.send({ result : "no user found"})
-        }
+    else {
+        res.send({ result: "no user found" })
+    }
 });
 
-app.post("/add-product" , async (req ,res) => {
-    let product  = new Product(req.body);
+app.post("/add-product", async (req, res) => {
+    let product = new Product(req.body);
     let result = await product.save();
     res.send(result)
 });
-app.get("/products", async (req , res) => {
+app.get("/products", async (req, res) => {
     let products = await Product.find();
-    if(products.length>0){
+    if (products.length > 0) {
         res.send(products)
-    }else{
-        res.send({result: "No Products found"})
+    } else {
+        res.send({ result: "No Products found" })
     }
 });
 
-app.delete("/product/:id" , async(req , res) => {
-    const result = await Product.deleteOne({_id:req.params.id})
+app.delete("/product/:id", async (req, res) => {
+    const result = await Product.deleteOne({ _id: req.params.id })
     res.send(result);
 });
 
-    app.get("/product/:id" , async (req, res) =>{
-        let result = await Product.findOne({_id:req.params.id});
-        if(result){
-            res.send(result)
-        }else{
-            res.send({result: "No Record found"})
-        }
-    })
+app.get("/product/:id", async (req, res) => {
 
-app.put("/product/:id" , async (req, res) => {
+    try {
+        console.log("hello", req)
+        let result = await Product.findById({ _id: req.params.id });
+        if (result) {
+            res.send(result)
+        } else {
+            res.send({ result: "No Record found" })
+        }
+    } catch (error) {
+        console.log(error.message)
+    }
+   
+})
+
+app.put("/product/:id", async (req, res) => {
     let result = await Product.updateOne(
-        {_id : req.params.id },
+        { _id: req.params.id },
         {
-            $set : req.body
+            $set: req.body
         }
     )
     res.send(result)
 })
 
-app.get("/search/:key" , async (req , res) => {
+app.get("/search/:key", async (req, res) => {
     let result = await Product.find({
         "$or": [
-            {name : { $regex: req.params.key} },
-            {company : { $regex: req.params.key} },
-            {category : { $regex: req.params.key}}
+            { name: { $regex: req.params.key } },
+            { company: { $regex: req.params.key } },
+            { category: { $regex: req.params.key } }
         ]
     });
     res.send(result)
